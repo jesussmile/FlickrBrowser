a2581ad9cb64dca0d5a580295df233589e14d36a
jesussmile <pullbackwild@gmail.com>
2016-02-03 20:21:08 +0545

    added flowerclass & GetFlickrJsonData

--- app/src/main/java/com/example/pannam/flickrbrowser/MainActivity.java
+++ app/src/main/java/com/example/pannam/flickrbrowser/MainActivity.java
@@ -4,12 +4,24 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    //for logging
+    private static final String LOG_TAG = "MainActivity";
+    //comes from Json parser
+    private List<Photo> mPhotoList = new ArrayList<Photo>();
+    private RecyclerView mRecyclerView ;
+    //adapter
+    private FlickrRecyclerViewAdapter flickrRecyclerViewAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,4 +61,33 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    //CREATE async task within the method
+    public class ProcessPhotos extends GetFlickrjsonData{
+
+        //initialize json data
+        public ProcessPhotos(String searchCriteria, boolean matchAll) {
+            super(searchCriteria, matchAll);
+        }
+
+        public void execute(){
+            super.execute();
+            ProcessData processData= new ProcessData();
+            processData.execute();
+        }
+
+        public class ProcessData extends DownloadJsonData{
+            @Override
+            protected void onPostExecute(String webData) {
+                super.onPostExecute(webData);
+                flickrRecyclerViewAdapter = new FlickrRecyclerViewAdapter(MainActivity.this,getMPhotos());
+                mRecyclerView.setAdapter(flickrRecyclerViewAdapter);
+            }
+        }
+    }
+
+
+
+
+
 }
