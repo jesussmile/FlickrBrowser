7c8462cc738e3e005a0ba55d2c73d0453b54646d
jesussmile <pullbackwild@gmail.com>
2016-02-05 20:06:56 +0545

    added RecyclerItemClickListener.java basically it just checks if its long tap or short tap etc.

---
+++ app/src/main/java/com/example/pannam/flickrbrowser/RecyclerItemClickListener.java
@@ -0,0 +1,67 @@
+package com.example.pannam.flickrbrowser;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by pannam on 2/5/2016.
+ */
+
+//created this class to open up a detailed view when a thumbnail is clicked
+    //all this code does is check for click or long click thats it!
+public class RecyclerItemClickListener implements RecyclerView.OnItemTouchListener {
+
+    public static interface OnItemClickListener {
+
+        public void onItemClick(View view, int position);
+
+        public void onItemLongClick(View view, int position);
+
+
+    }
+
+    private OnItemClickListener mListener;
+    private GestureDetector mGestureDetector;
+
+    public RecyclerItemClickListener(Context context, final RecyclerView recyclerView, OnItemClickListener listener) {
+
+        mListener = listener;
+        mGestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
+            public boolean onSingleTapUp(MotionEvent e) {
+                return true;
+            }
+
+            public void onLongPress(MotionEvent e) {
+                View childView = recyclerView.findChildViewUnder(e.getX(), e.getY());
+                if (childView != null && mListener != null) {
+                    mListener.onItemLongClick(childView, recyclerView.getChildPosition(childView));
+                }
+
+            }
+        });
+
+    }
+
+    public boolean onInterceptTouchEvent(RecyclerView view, MotionEvent e) {
+        View childView = view.findChildViewUnder(e.getX(), e.getY());
+        if (childView != null && mListener != null && mGestureDetector.onTouchEvent(e)) {
+            mListener.onItemClick(childView, view.getChildPosition(childView));
+
+        }
+
+        return false;
+    }
+
+    public void onTouchEvent(RecyclerView view, MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+    }
+
+}
